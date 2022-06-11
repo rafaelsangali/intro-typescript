399f0c8f06d66ea54d6969c0e8d88553fa3b0e7b
Rafael Sangali <rafaelsangali@outlook.com>
2022-06-11 10:16:39 -0300

    estudando typescript

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,51 @@
 "use strict";
+//number
 let x = 10;
-x = 20;
-console.log(x);
+//string
+let y = 'Hello World';
+//boolean
+let z = false;
+//array string
+let a = ['João', 'Maria', 'Lucas'];
+// Interface de array 
+const b = ['Patrica', 'Adriano'];
+// Tuple (define posições e quantidades além do tipo)
+// Verifica a quantidade e o tipo
+let c;
+c = ['cachorro', 'gato', 14, true];
+// Enum - Mapameamento de info, define valores como objetos
+var StatusAprovacao;
+(function (StatusAprovacao) {
+    StatusAprovacao[StatusAprovacao["Aprovado"] = 0] = "Aprovado";
+    StatusAprovacao[StatusAprovacao["Pendente"] = 1] = "Pendente";
+    StatusAprovacao[StatusAprovacao["Reprovado"] = 2] = "Reprovado";
+})(StatusAprovacao || (StatusAprovacao = {}));
+const statusDaAprovacao = StatusAprovacao.Aprovado;
+// Any - ignora a tipagem
+const infosApi = [true, 'oasuidn', 234,];
+// Void - função que não retorna nada
+function teste(msg) {
+    console.log(msg);
+}
+//Null e Undefined
+const u = undefined;
+const n = null;
+// Objetc
+function novaFuncao(object) {
+}
+novaFuncao({
+    id: 2,
+    nome: 'oaidunbaoisd',
+});
+// Never (looping)
+function loop() {
+    while (true) {
+        console.log('infinito e além');
+    }
+}
+// Union Types
+function notas(nota) {
+    console.log(`sua nota é ${nota}`);
+}
+notas(10);
+notas('10');
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,62 @@
-let x:number = 10
+//number
+let x:number = 10;
+//string
+let y:string = 'Hello World';
 
-x = 20;
+//boolean
+let z:boolean = false;
 
-console.log(x);
+//array string
+let a:string[] = ['João','Maria','Lucas'];
+
+// Interface de array 
+const b: Array<string> = ['Patrica','Adriano'];
+
+// Tuple (define posições e quantidades além do tipo)
+// Verifica a quantidade e o tipo
+let c: [string,string,number, boolean];
+c = ['cachorro','gato', 14, true]
+
+// Enum - Mapameamento de info, define valores como objetos
+enum StatusAprovacao{
+    Aprovado,
+    Pendente,
+    Reprovado,
+}
  
+const statusDaAprovacao: StatusAprovacao = StatusAprovacao.Aprovado
+
+// Any - ignora a tipagem
+const infosApi: any[] = [true, 'oasuidn', 234,];
+
+// Void - função que não retorna nada
+function teste(msg: string): void {
+    console.log(msg);
+}
+
+//Null e Undefined
+const u:undefined = undefined
+const n:null = null
+
+// Objetc
+function novaFuncao(object: object){
+}
+
+novaFuncao({
+    id: 2,
+    nome: 'oaidunbaoisd',
+})
+
+// Never (looping)
+function loop():never{
+    while(true){
+        console.log('infinito e além');
+    }
+}
+
+// Union Types
+function notas(nota: number | string){
+    console.log(`sua nota é ${nota}`)
+}
+notas(10);
+notas('10');
